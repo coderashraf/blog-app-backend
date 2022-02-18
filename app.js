2ce318fec1b8244a86ecb6d12b1459cd4b5d4138
coderashraf <ashrafulbnt@gmail.com>
2022-02-18 15:45:48 +0530

    port

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const cors = require("cors");
 const routerFile = require("./routes");
 
 const app = express();
+
+let port =process.env.PORT||8000;
+
 app.use(cors());
 app.use("/api", routerFile);
-app.listen(8000, () => {
+app.listen(port, () => {
   console.log("Application has started running successfully at port 8000");
 });
